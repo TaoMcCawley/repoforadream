ae1f59fc05d576ab0253ef9fdf205a7b9f745bf8
Adrian Smith <adriansmith5796@gmail.com>
2018-02-06 17:17:54 -0500

    Started User class

---
+++ model/User.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adriansmith
+ * Date: 2/6/18
+ * Time: 2:14 PM
+ */
+
+class User
+{
+    private $_username;
+    private $_password;
+
+    function __construct()
+    {
+
+    }
+}
