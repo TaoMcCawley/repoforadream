2d57ba275d0c49c45ac313e87ff6f8e7cec7b612
Adrian Smith <adriansmith5796@gmail.com>
2018-02-20 16:33:36 -0500

    Now uses classes and reroutes if sign up was successful

--- index.php
+++ index.php
@@ -7,14 +7,55 @@
  */
 
 require_once 'vendor/autoload.php';
+include '../../../errors.php';
+session_start();
 
 $f3 = Base::instance();
 
 $f3->set('DEBUG', 3);
 
-$f3->route('GET /', function($f3){
+$f3->route('GET|POST /', function($f3){
+    $f3->set('title', 'Keyboard - Login');
+
+    if(isset($_POST['signupsubmit'])){
+        $name = $_POST['name'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+
+        $user = new User($username, $password, $name, $email);
+        $dbh = new UserDB($f3);
+
+        if($password !== $_POST['retype']) {
+            $user->addError('retype', ' * Password entries must match.');
+        }
+        if (!$dbh->exists($user)){
+            $dbh->signup($user);
+            $_SESSION['user'] = $user;
+            $f3->reroute('login');
+        }else{
+            if($dbh->checkEmail($user)) {
+                $user->addError('duplicateEmail', 'An account with that email already exists.');
+            }
+            if($dbh->checkUsername($user)){
+                $user->addError('duplicateUsername', 'An account with that username already exists.');
+            }
+        }
+
+
+        $f3->set('username', $user->getUsername());
+        $f3->set('email', $user->getEmail());
+        $f3->set('name', $user->getName());
+        $f3->set('errors', $user->getErrors());
+    }
+
     $template = new Template();
     echo $template->render('view/login.html');
+});
+
+$f3->route('GET /login', function(){
+    $template = new Template();
+    echo $template->render('view/header.html');
 });
 
 $f3->run();
